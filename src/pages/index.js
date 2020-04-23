669016fd84ca3f3a61c78a1927d3be7147ab383e
douglasmccarroll <org.git.001@brightworks.com>
2020-04-23 11:24:23 -0400

    GraphQL queries successfully accessing DynamoDB data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import withRoot from "../utils/withRoot";
-import React, { forwardRef } from "react";
-import { graphql } from "gatsby";
+import React, {forwardRef} from "react";
+import {graphql} from "gatsby";
 import SEO from "../components/SEO";
 import Page from "../components/Page";
 import withStyles from "@material-ui/styles/withStyles";
-import Amplify from "aws-amplify";
+import Amplify, {API, graphqlOperation} from "aws-amplify";
 import config from "../aws-exports";
-import { withAuthenticator } from "aws-amplify-react";
+import {withAuthenticator} from "aws-amplify-react";
 import MaterialTable from "material-table";
 import AddBox from "@material-ui/icons/AddBox";
 import ArrowDownward from "@material-ui/icons/ArrowDownward";
@@ -23,6 +23,7 @@ import Remove from "@material-ui/icons/Remove";
 import SaveAlt from "@material-ui/icons/SaveAlt";
 import Search from "@material-ui/icons/Search";
 import ViewColumn from "@material-ui/icons/ViewColumn";
+import * as queries from "../graphql/queries";
 
 Amplify.configure(config);
 
@@ -31,10 +32,82 @@ const styles = () => ({
     fontWeight: "bold",
   },
 });
+
+const listUsers = `query ListUsers(
+  $filter: ModelUserFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      groupID
+      userName
+      firstName
+      lastName
+      fullName
+      emailAddress
+      phoneNumber
+      createdDate
+      actions {
+        items {
+          dateTime
+          details
+          id
+          userID
+        }
+      }
+    }
+    nextToken
+  }
+}
+`;
+
 const Home = () => { // (props) => {
+  API.graphql(graphqlOperation(queries.listGroups))
+    .then(
+      groupsData => {
+        console.log("****************************");
+        console.log(groupsData);
+        console.log("****");
+        const group = groupsData.data.listGroups.items[0];
+        const groupId = group.id;
+        console.log("Group ID: " + groupId);
+
+        API.graphql(graphqlOperation(listUsers, {
+          filter: {
+            groupID: {
+              eq: groupId
+            }
+          }
+        }))
+          .then(
+            usersData => {
+              console.log("************* Users ***************");
+              console.log(usersData);
+              console.log("****");
+              for (let i = 0; i < usersData.data.listUsers.items.length; i++) {
+                let user = usersData.data.listUsers.items[i];
+                console.log("************* User ***************");
+                console.log(user);
+                console.log("****");
+                console.log("************* Actions ***************");
+                for (let j = 0; j < user.actions.items.length; j++) {
+                  let action = user.actions.items[j];
+                  let detailsJSON = action.details;
+                  let details = JSON.parse(detailsJSON);
+                  console.log(details.actionType);
+                }
+                console.log("****");
+              }
+            }
+          );
+      }
+    );
+
   // const products = props.data.allMarkdownRemark.edges;
   return (
-    <Page >
+    <Page>
       <SEO title="Home">
         <meta
           content="An experimental website ..."
@@ -45,21 +118,21 @@ const Home = () => { // (props) => {
       <MaterialTable
         icons={tableIcons}
         columns={[
-          { title: "First Name", field: "name" },
-          { title: "Last Name", field: "surname" },
-          { title: "Birth Year", field: "birthYear", type: "numeric" },
+          {title: "First Name", field: "name"},
+          {title: "Last Name", field: "surname"},
+          {title: "Birth Year", field: "birthYear", type: "numeric"},
           {
             title: "Birth City",
             field: "birthCity",
-            lookup: { 34: "İstanbul", 63: "Şanlıurfa" }
+            lookup: {34: "İstanbul", 63: "Şanlıurfa"}
           }
         ]}
         data={[
-          { name: "Mehmet", surname: "Baran", birthYear: 1987, birthCity: 63 }
+          {name: "Mehmet", surname: "Baran", birthYear: 1987, birthCity: 63}
         ]}
         options={{
-          pageSize: 8,
-          rowStyle: { height: "30px" }
+          pageSize: 10, // Only 5, 10 or 20 - otherwise you will get a warning in the console
+          rowStyle: {height: "30px"}
         }}
         title="Comedy and Language Learning"
       />
@@ -100,27 +173,27 @@ export const query = graphql`
 `;
 
 const tableIcons = {
-  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref}/>),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref}/>),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref}/>),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref}/>),
+  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref}/>),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref}/>),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref}/>),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref}/>),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref}/>),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref}/>),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref}/>),
+  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref}/>),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref}/>),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref}/>),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref}/>),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref}/>),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref}/>)
 };
 
 Home.propTypes = {
   // classes: PropTypes.object.isRequired,
 };
 
-export default withAuthenticator(withRoot(withStyles(styles)(Home)), { includeGreetings: true });
+export default withAuthenticator(withRoot(withStyles(styles)(Home)), {includeGreetings: true});
